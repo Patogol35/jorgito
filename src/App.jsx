6831b7ebad2b61ccbf78f16168f805cb4da449da
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-05 10:19:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// App.jsx
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import Projects from "./components/Projects.jsx";
+import Certifications from "./components/Certifications.jsx";
 import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 import { Box } from "@mui/material";
@@ -14,19 +14,23 @@ function App() {
       <Navbar />
       <Hero />
 
-      <Box id="about" sx={{ bgcolor: "#f9f9f9", py: 10 }}>
+      <Box id="about" sx={{ py: 10 }}>
         <About />
       </Box>
 
-      <Box id="skills" sx={{ bgcolor: "white", py: 10 }}>
+      <Box id="skills" sx={{ py: 10 }}>
         <Skills />
       </Box>
 
-      <Box id="projects" sx={{ bgcolor: "#f9f9f9", py: 10 }}>
+      <Box id="projects" sx={{ py: 10 }}>
         <Projects />
       </Box>
 
-      <Box id="contact" sx={{ bgcolor: "white", py: 10 }}>
+      <Box id="certifications" sx={{ py: 10 }}>
+        <Certifications />
+      </Box>
+
+      <Box id="contact" sx={{ py: 10 }}>
         <Contact />
       </Box>
 
