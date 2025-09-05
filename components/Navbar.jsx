8fad2723ae45d7e2ddb6d3b5ab37b6b4ebc8fc4b
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-04 21:38:31 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,1 +1,16 @@
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
 
+export default function Navbar() {
+  return (
+    <AppBar position="sticky" color="primary" sx={{ mb: 4 }}>
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Andres Gamer
+        </Typography>
+        <Button color="inherit" href="#about">Sobre mí</Button>
+        <Button color="inherit" href="#projects">Proyectos</Button>
+        <Button color="inherit" href="#contact">Contacto</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
