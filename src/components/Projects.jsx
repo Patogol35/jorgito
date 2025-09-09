7c153613d053ba0cb622a9dfab9ddf084c87baf4
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-09 09:36:19 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -58,7 +58,7 @@ export default function Projects() {
             variant="h6"
             sx={{
               fontWeight: "bold",
-              color: theme.palette.mode === "dark" ? "#90caf9" : "#1976d2",
+              color: theme.palette.mode === "dark" ? "#bbdefb" : "#1976d2",
             }}
           >
             Proyectos
