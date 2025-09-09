1ba4083f369f61dd83da9c616c16a20b167afb53
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-09 08:51:11 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ export default function Projects() {
         initial={{ opacity: 0, scale: 0.8 }}
         whileInView={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8 }}
-        style={{ textAlign: "center", marginBottom: "1rem" }}
+        style={{ textAlign: "center", marginBottom: "2rem" }}
       >
         <Box
           sx={{
