107eccd9f3a2625f527a584d3f19a8108b745805
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-29 18:31:46 -0500

    Update ChatBot.jsx

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -722,7 +722,7 @@ const isAboutOwner = (text) => {
     "quien es", "quién es", "formacion", "formación",
     "educacion", "educación", "máster", "master",
     "libros", "libro", "full stack", "desarrollador",
-    "ingeniero", "stack","full","contactar", "contacto", "full stack,""whatsapp"
+    "ingeniero", "stack","full","contactar", "contacto","whatsapp"
   ];
 
   const hasSensitive = sensitiveKeywords.some(kw => normalizedText.includes(kw));
