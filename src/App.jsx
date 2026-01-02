1f2df1ae42e8dfdf22b622225b4108655d28aac3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-02 16:45:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
                 mb: 4,
                 p: { xs: 3, md: 6 },
                 borderRadius: 3,
-                borderLeft: `5px solid ${color}`,
+                borderLeft: `3px solid ${color}`,
                 scrollMarginTop: scrollOffset,
                 transition: "all 0.3s ease",
                 "&:hover": {
