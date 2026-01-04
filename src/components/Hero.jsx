0547b5e533ab9899e2f406c5095641627820ead0
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-04 18:54:20 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -112,7 +112,7 @@ export default function Hero({ mode, setMode }) {
               filter: "blur(0px)",
             }}
             transition={{
-              duration: 2.0,
+              duration: 2.5,
               ease: easeOutExpo,
             }}
             style={{
