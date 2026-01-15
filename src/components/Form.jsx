318691fc00f88c996e48d080643cca3dfc35b0cf
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-15 10:11:59 -0500

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -81,7 +81,7 @@ export default function Contact() {
             textAlign="center"
             sx={{
               color: theme.palette.primary.main,
-              mb: 0.3, // ⬅️ MENOS SEPARACIÓN
+              mb: 0.1, // ⬅️ MENOS SEPARACIÓN
               fontSize: { xs: "2rem", sm: "2.3rem" },
             }}
           >
@@ -91,7 +91,7 @@ export default function Contact() {
           <Typography
             textAlign="center"
             sx={{
-              mb: 2.5, // ⬅️ MÁS COMPACTO
+              mb: 3.5, // ⬅️ MÁS COMPACTO
               color: "text.secondary",
               fontStyle: "italic",
             }}
