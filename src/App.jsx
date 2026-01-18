9ca27b56a9af341d84b0889e8043f508ee441f6f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-18 10:22:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
           {[
             { id: "about", color: "#2e7d32", Component: About },
             { id: "skills", color: "#fb8c00", Component: Skills },
-            { id: "certifications", color: "#1976d2", Component: Certifications },
+            { id: "certifications", color:"#f9a825", Component: Certifications },
             { id: "projects", color: "#1976d2", Component: Projects },
             { id: "contact", color: "#d32f2f", Component: Contact },
           { id: "form", color: "#00897b", Component: Form, minHeight: "100vh" },
