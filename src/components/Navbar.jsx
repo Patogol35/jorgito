6c525b7de8ee846dd88fd1a875ba9d1b712ac3d6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-18 07:28:06 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const menuItems = [
   {
     label: "Certificados",
     href: "#certifications",
-    color: "linear-gradient(135deg, #f9a825, #fdd835)"
+    color: "linear-gradient(135deg, #f9a825, #fdd835)",
     icon: <VerifiedIcon sx={{ color: "#fff" }} />,
   },
   {
