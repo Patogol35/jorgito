e6552d018b58bc4883ab958128229103d435cd41
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-30 07:38:39 -0500

    Update useSkillsFilter.js

--- src/hooks/useSkillsFilter.js
+++ src/hooks/useSkillsFilter.js
@@ -1,25 +1,10 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useState } from "react";
 
 export function useSkillsFilter() {
   const [filter, setFilter] = useState("All");
 
   const containerRef = useRef(null);
   const buttonRefs = useRef({});
-
-  useEffect(() => {
-    const activeBtn = buttonRefs.current[filter];
-    const container = containerRef.current;
-
-    if (activeBtn && container) {
-      container.scrollTo({
-        left:
-          activeBtn.offsetLeft -
-          container.offsetWidth / 2 +
-          activeBtn.offsetWidth / 2,
-        behavior: "smooth",
-      });
-    }
-  }, [filter]);
 
   return {
     filter,
