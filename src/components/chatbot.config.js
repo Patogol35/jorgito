400547b43e39ec6d3659f62e8a73a1f98c68f727
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-03-26 06:51:22 -0500

    Update chatbot.config.js

--- src/components/chatbot.config.js
+++ src/components/chatbot.config.js
@@ -151,7 +151,7 @@ export const INTENTS = {
   FAREWELL: ["adiós", "hasta luego", "bye", "chao"],
 
   GREETING: ["hola", "buenas", "buenos días"],
-  PROFILE: ["jorge", "perfil"],
+  PROFILE: ["jorge", "perfil", "quien es"],
   EDUCATION: ["estudios", "máster", "formación", "estudio","formacion", "educación", "educacion"],
   EXPERIENCE: ["experiencia"],
   SKILLS: ["tecnologías", "lenguajes", "habilidades","tecnología"],
