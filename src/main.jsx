55adcdbd399735631bd95c28379b51ac82b6a105
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-05-01 16:24:44 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 // main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App.jsx";
-import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
