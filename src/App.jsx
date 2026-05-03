cab162796dacc4b9d2e2cfffea314f9a15d1bce3
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-05-03 15:10:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function App() {
                     ? "linear-gradient(rgba(255,255,255,0.03), rgba(255,255,255,0.03))"
                     : "linear-gradient(rgba(0,0,0,0.015), rgba(0,0,0,0.015))",
 
-                border: `1.5px solid ${color}33`,
+                border: `1.5px solid ${color}55`,
 
                 boxShadow:
                   theme.palette.mode === "light"
