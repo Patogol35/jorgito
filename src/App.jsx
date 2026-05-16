58620112e61079a964350d8658eb7841b5a0eaaf
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-05-16 08:01:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -156,19 +156,20 @@ function App() {
         />
 
         <Container
-          maxWidth="lg"
-          disableGutters
-          sx={{
-            py: 6,
-            px: { xs: 2, sm: 4, md: 6, lg: 8, xl: 12 },
-          }}
-        >
+  maxWidth={false}
+  disableGutters
+  sx={{
+    py: 6,
+    px: { xs: 2, sm: 4, md: 6, lg: 8, xl: 10 },
+  }}
+>
           {sections.map(({ id, color, Component }) => (
             <Paper
               key={id}
               id={id}
               elevation={0}
               sx={(theme) => ({
+                width: "100%",
                 mb: 4,
                 p: { xs: 3, md: 5 },
                 borderRadius: { xs: 3, md: 4 },
