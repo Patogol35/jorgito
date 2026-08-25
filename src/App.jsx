c489d190e19046b1729a983e14d809250df7d7d7
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-24 19:39:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -172,15 +172,10 @@ function App() {
                 p: { xs: 3, md: 5 },
                 borderRadius: "18px",
 
-                backgroundColor:
+                background:
   theme.palette.mode === "dark"
     ? "#121212"
-    : "#ffffff",
-
-                backgroundImage:
-  theme.palette.mode === "dark"
-    ? "linear-gradient(rgba(255,255,255,0.03), rgba(255,255,255,0.03))"
-    : "linear-gradient(135deg, #ffffff 0%, #eef6ff 100%)",
+    : "linear-gradient(135deg, #ffffff 0%, #eaf4ff 100%)",
                 
                 border: `1.5px solid ${color}55`,
 
