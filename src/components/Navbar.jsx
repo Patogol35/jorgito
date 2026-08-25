7d90809d83242005e726c61b6d1fc337a442bcec
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-24 23:37:19 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,12 +54,13 @@ const getMenuItems = (t) => [
     color: "linear-gradient(135deg, #f57c00, #ffb74d)",
     icon: <BuildIcon sx={{ color: "#fff" }} />,
   },
-  {
-  label: t.menu.certifications,
-  href: "#certifications",
-  color: "linear-gradient(135deg, #455a64, #78909c)",
-  icon: <VerifiedIcon sx={{ color: "#fff" }} />,
-},
+    {
+    label: t.menu.certifications,
+    href: "#certifications",
+    color:
+      "linear-gradient(135deg, rgba(0,0,0,0.25), rgba(0,0,0,0.25)), linear-gradient(135deg, #CBB26A, #B59A5A)",
+    icon: <VerifiedIcon sx={{ color: "#fff" }} />,
+  },
   
   {
     label: t.menu.contact,
