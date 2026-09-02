b35906cc26c6f66744dbaadf1bb56d3e21ac536e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-02 06:44:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -242,7 +242,7 @@ function App() {
     onClick={() => setTerminalOpen(true)}
     sx={(theme) => ({
       position: "fixed",
-      bottom: 280,
+      bottom: 380,
       right: 16,
       zIndex: 1200,
       bgcolor:
