89b8f58bdf47c1879f5e91420549ef63815f25c4
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-04 17:33:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,10 +103,45 @@ function App() {
           },
         },
         typography: {
-          fontFamily: "Poppins, Roboto, sans-serif",
-          h2: { fontWeight: 700 },
-          h4: { fontWeight: 600 },
-        },
+  fontFamily: "Roboto, sans-serif",
+
+  h1: {
+    fontWeight: 700,
+  },
+
+  h2: {
+    fontWeight: 700,
+  },
+
+  h3: {
+    fontWeight: 700,
+  },
+
+  h4: {
+    fontWeight: 600,
+  },
+
+  h5: {
+    fontWeight: 600,
+  },
+
+  h6: {
+    fontWeight: 500,
+  },
+
+  body1: {
+    fontWeight: 400,
+  },
+
+  body2: {
+    fontWeight: 400,
+  },
+
+  button: {
+    fontWeight: 500,
+    textTransform: "none",
+  },
+},
         shape: {
           borderRadius: 12,
         },
