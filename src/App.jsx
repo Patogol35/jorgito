d983e907e1958c266157d5ae0e431e9cdb708140
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-03 22:24:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -203,7 +203,7 @@ function App() {
       )
     `,
                 
-                border: `0.5px solid ${color}55`,
+                border: `1px solid ${color}55`,
 
                 boxShadow:
                   theme.palette.mode === "light"
@@ -218,7 +218,7 @@ function App() {
 
                 "&:hover": {
                   transform: "translateY(-4px) scale(1.01)",
-                  border: `1.5px solid ${color}`,
+                  border: `1px solid ${color}`,
                   boxShadow:
                     theme.palette.mode === "light"
                       ? "0 10px 24px rgba(0,0,0,0.08)"
