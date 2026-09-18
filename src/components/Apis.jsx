e3e53f0918c6c0c4afd0cdcd5553f79f4d5b388e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-17 21:46:22 -0500

    Create Apis.jsx

---
+++ src/components/Apis.jsx
@@ -0,0 +1,28 @@
+Api de apoyo para el chatbot del portafolio 
+Render jorgepatriciosantamaria@gmail.com
+URLs https://chatbot-groq-yjdy.onrender.com
+Uptimerobot: patogol3737@gmail.com
+
+  
+
+Chatbot del portafolio
+
+Back en render con este correo
+patricio_jorge@hotmail.es
+
+URLs
+https://groqbot-08r8.onrender.com
+
+Tráfico de red uptimerobot  patricio_jorge@hotmail.es
+
+
+
+
+  
+patogol3636@gmail.com está cuenta de render es para el proyecto aparte del chatbot
+
+Api de groq:
+ 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
+
+tráfico de red uptime robot con el correo patogol3636@gmail.com y la URLs es 
+https://chatbot-local-groq.onrender.com
